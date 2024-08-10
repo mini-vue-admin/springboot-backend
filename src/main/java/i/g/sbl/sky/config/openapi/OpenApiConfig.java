3d5da29d567a2f43fe21ba0c8ef7273649c12a92
shenb <shenbinglife@163.com>
2024-08-10 22:27:34 +0800

    修复openapi对日期的显示

--- src/main/java/i/g/sbl/sky/config/openapi/OpenApiConfig.java
+++ src/main/java/i/g/sbl/sky/config/openapi/OpenApiConfig.java
@@ -2,11 +2,21 @@ package i.g.sbl.sky.config.openapi;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Schema;
+import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Configuration
 public class OpenApiConfig {
+    static {
+        var schema = new Schema<LocalDateTime>();
+        schema.example(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).type("string");
+        SpringDocUtils.getConfig().replaceWithSchema(LocalDateTime.class, schema);
+    }
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
