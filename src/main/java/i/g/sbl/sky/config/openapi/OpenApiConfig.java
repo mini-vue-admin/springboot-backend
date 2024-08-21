225d34b79410aa1d8236ae07d769bbff534dce74
shenbing <shenbinglife@163.com>
2024-08-21 14:07:37 +0000

    openapi接口分组

--- src/main/java/i/g/sbl/sky/config/openapi/OpenApiConfig.java
+++ src/main/java/i/g/sbl/sky/config/openapi/OpenApiConfig.java
@@ -3,6 +3,7 @@ package i.g.sbl.sky.config.openapi;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Schema;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springdoc.core.utils.SpringDocUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +18,37 @@ public class OpenApiConfig {
         schema.example(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))).type("string");
         SpringDocUtils.getConfig().replaceWithSchema(LocalDateTime.class, schema);
     }
+
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-                .info(new Info().title("SkyProbe").version("v1"));
+                .info(new Info()
+                        .title("SkyProbe")
+                        .description("问天测试平台")
+                        .version("v1"));
+    }
 
+    @Bean
+    public GroupedOpenApi authApi() {
+        return GroupedOpenApi.builder()
+                .group("认证鉴权")
+                .pathsToMatch("/login")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi commonApi() {
+        return GroupedOpenApi.builder()
+                .group("公共接口")
+                .pathsToMatch("/genCode", "/healthy")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi systemApi() {
+        return GroupedOpenApi.builder()
+                .group("系统管理")
+                .pathsToMatch("/system/**")
+                .build();
     }
 }
--- src/main/java/i/g/sbl/sky/controller/CommonController.java
+++ src/main/java/i/g/sbl/sky/controller/CommonController.java
@@ -1,9 +1,11 @@
 package i.g.sbl.sky.controller;
 
+import i.g.sbl.sky.basic.model.ResponseData;
 import i.g.sbl.sky.basic.utils.CodeGenerator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,12 @@ import javax.sql.DataSource;
 public class CommonController {
     @Autowired
     private DataSource dataSource;
+
+    @Operation(summary = "健康状态")
+    @GetMapping("healthy")
+    public ResponseData<Void> healthy() {
+        return ResponseData.success();
+    }
 
     @Operation(summary = "genCode")
     @PostMapping("genCode")
